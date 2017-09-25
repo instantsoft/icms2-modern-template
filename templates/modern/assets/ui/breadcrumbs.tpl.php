5d9b35c61bc006b2f75cb342bd8cee5e09224b8f
atid <uzunhair@yandex.ru>
2017-09-25 14:08:10 +0300

    Изменены хлебные крошки

--- templates/modern/assets/ui/breadcrumbs.tpl.php
+++ templates/modern/assets/ui/breadcrumbs.tpl.php
@@ -1,19 +1,19 @@
 <?php $listed = array(); ?>
-<ul>
+<ol class="breadcrumb">
 
-    <li class="home">
-        <a href="<?php echo $options['home_url']; ?>" title="<?php echo LANG_HOME; ?>"></a>
+    <li class="breadcrumb-item" home">
+        <a href="<?php echo $options['home_url']; ?>" title="<?php echo LANG_HOME; ?>">
+            <i class="fa fa-home"></i>
+        </a>
     </li>
 
     <?php if ($breadcrumbs) { ?>
-
-        <li class="sep"></li>
 
         <?php foreach($breadcrumbs as $id=>$item){ ?>
 
             <?php if (in_array($item['href'], $listed)){ continue; } ?>
 
-            <li <?php if (!isset($item['is_last'])){ ?>itemscope itemtype="http://data-vocabulary.org/Breadcrumb"<?php } ?>>
+            <li class="breadcrumb-item <?php if (isset($item['is_last'])){ ?>active<?php } ?>" <?php if (!isset($item['is_last'])){ ?>itemscope itemtype="http://data-vocabulary.org/Breadcrumb"<?php } ?>>
                 <?php if (!isset($item['is_last'])){ ?>
                     <a href="<?php html($item['href']); ?>" itemprop="url"><span itemprop="title"><?php html($item['title']); ?></span></a>
                 <?php } else { ?>
@@ -21,13 +21,9 @@
                 <?php } ?>
             </li>
 
-            <?php if ($id < sizeof($breadcrumbs)-1){ ?>
-                <li class="sep"></li>
-            <?php } ?>
-
             <?php $listed[] = $item['href']; ?>
 
         <?php } ?>
 
     <?php } ?>
-</ul>
+</ol>
