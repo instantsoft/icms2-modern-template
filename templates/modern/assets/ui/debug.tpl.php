56e528ddd45984d309060985a2f36866cabbea5c
Loadir <idanilkoff@yandex.ru>
2018-07-10 21:30:55 +0500

    Хлебные крошки, отладка, фильтр-панель, сворачиваемая

--- templates/modern/assets/ui/debug.tpl.php
+++ templates/modern/assets/ui/debug.tpl.php
@@ -2,54 +2,89 @@
 $points_data = cmsDebugging::getPointsData();
 $points_tab = cmsDebugging::getPointsTargets();
 ?>
-<div id="debug_widget" class="modal fade" role="dialog" aria-hidden="true">
-    <div class="modal-dialog modal-lg" role="document">
-        <div class="modal-content">
-            <div class="modal-body">
-                <ul class="nav nav-tabs mb-3" role="tablist">
-                    <?php foreach($points_tab as $tab_name => $tab) { ?>
-                        <li class="nav-item">
-                            <a class="nav-link<?php echo $tab_name == 'db' ? ' active': ''; ?>"
-                               id="debug-<?php echo $tab_name; ?>-tab"
-                               data-toggle="tab"
-                               href="#debug-<?php echo $tab_name; ?>"
-                               role="tab"
-                               aria-controls="debug-<?php echo $tab_name; ?>"
-                               aria-selected="<?php echo $tab_name == 'db' ? 'true': 'false'; ?>">
-                                <?php echo $tab['title']; ?><?php echo $tab['count'] ? ' <sup>' . $tab['count'] . '</sup>' : ''; ?>
-                            </a>
-                        </li>
-                    <?php } ?>
-                </ul>
-                <div class="tab-content">
-                <?php foreach($points_data as $tab_name => $data) { ?>
-                    <div class="tab-pane fade<?php echo $tab_name == 'db' ? ' show active': ''; ?>" id="debug-<?php echo $tab_name; ?>" role="tabpanel" aria-labelledby="debug-<?php echo $tab_name; ?>-tab">
-                        <div class="queries_wrap">
-                            <?php foreach($data as $query) { ?>
-                                <div class="query">
-                                    <div class="src"><?php echo $query['src']; ?></div>
-                                    <?php if($query['data']){ ?>
-                                        <div class="debug_data">
-                                            <?php echo isset($query['data_callback']) ? $query['data_callback']($query['data']) : nl2br(htmlspecialchars($query['data'])); ?>
-                                        </div>
-                                    <?php } ?>
-                                    <?php if($query['time']){ ?>
-                                        <div class="query_time">
-                                            <?php echo LANG_DEBUG_QUERY_TIME; ?>
-                                            <span class="<?php echo (($query['time']>=0.1) ? 'red_query' : 'green_query'); ?>">
-                                                <?php echo $query['time']; ?>
-                                            </span>
-                                            <?php echo LANG_SECOND10 ?>
-                                        </div>
-                                    <?php } ?>
-                                </div>
-                            <?php } ?>
-                        </div>
-                    </div>
+
+<div id="debug_widget" class="modal-dialog modal-lg debug-widget" role="document">
+
+    <div class="modal-content">
+
+        <div class="modal-body">
+
+            <ul class="nav nav-tabs mb-3" role="tablist">
+
+                <?php foreach($points_tab as $tab_name => $tab) { ?>
+
+                    <li class="nav-item">
+
+                        <a class="nav-link<?php echo $tab_name == 'db' ? ' active': ''; ?>"
+                           id="debug-<?php echo $tab_name; ?>-tab"
+                           data-toggle="tab"
+                           href="#debug-<?php echo $tab_name; ?>"
+                           role="tab"
+                           aria-controls="debug-<?php echo $tab_name; ?>"
+                           aria-selected="<?php echo $tab_name == 'db' ? 'true': 'false'; ?>">
+                            <?php echo $tab['title']; ?><?php echo ' <sup>' . (int) $tab['count'] . '</sup>'; ?>
+                        </a>
+
+                    </li>
+
                 <?php } ?>
+
+            </ul>
+
+            <div class="tab-content">
+
+            <?php foreach($points_data as $tab_name => $data) { ?>
+
+                <div class="tab-pane fade<?php echo $tab_name == 'db' ? ' show active': ''; ?>" id="debug-<?php echo $tab_name; ?>" role="tabpanel" aria-labelledby="debug-<?php echo $tab_name; ?>-tab">
+
+                    <div class="list-group queries_wrap">
+
+                        <?php foreach($data as $query) { ?>
+
+                            <div class="list-group-item list-group-item-action query">
+
+                                <div class="src"><small><?php echo $query['src']; ?></small></div>
+
+                                <?php if($query['data']){ ?>
+
+                                    <div class="debug_data">
+
+                                        <?php echo isset($query['data_callback']) ? $query['data_callback']($query['data']) : '<code>' . nl2br(htmlspecialchars($query['data'])) . '</code>'; ?>
+
+                                    </div>
+
+                                <?php } ?>
+
+                                <?php if($query['time']){ ?>
+
+                                    <div class="query_time"><small>
+
+                                        <?php echo LANG_DEBUG_QUERY_TIME; ?>
+
+                                        <span class="badge <?php echo (($query['time']>=0.1) ? 'badge-danger' : 'badge-success'); ?>">
+                                            <?php echo $query['time']; ?>
+                                        </span>
+
+                                        <?php echo LANG_SECOND10 ?>
+
+                                    </small></div>
+
+                                <?php } ?>
+
+                            </div>
+
+                        <?php } ?>
+
+                    </div>
+
                 </div>
+
+            <?php } ?>
+
             </div>
+
         </div>
+
     </div>
 
 </div>
--- templates/modern/assets/ui/filter-panel.tpl.php
+++ templates/modern/assets/ui/filter-panel.tpl.php
@@ -1,76 +1,111 @@
 <?php $user = cmsUser::getInstance(); ?>
+
 <?php if (!isset($is_expanded)){ $is_expanded = false; } unset($filters['user_id']); ?>
+
 <?php $form_url = is_array($page_url) ? $page_url['base'] : $page_url; $form_url_sep = strpos($form_url, '?') === false ? '?' : '&'; ?>
-<div class="filter-panel gui-panel <?php echo $css_prefix;?>-filter mb-3">
+
+<div class="mb-3 filter-panel <?php echo $css_prefix;?>-filter">
+
     <div class="card">
-        <a class="filter-link card-header text-dark" data-toggle="collapse" href="#toggleFilter" aria-expanded="false" aria-controls="toggleFilter">
-            <span><?php echo LANG_SHOW_FILTER; ?></span>
+
+        <a class="card-header" role="button" data-toggle="collapse" href=".filter-collapsed" aria-expanded="true" aria-controls="toggleFilter">
+
+            <span class="collapse<?php if(!$filters && !$is_expanded){ ?> show<?php } ?> filter-collapsed"><?php echo LANG_SHOW_FILTER; ?></span>
+
+            <span class="collapse<?php if($filters || $is_expanded){ ?> show<?php } ?> filter-collapsed close" aria-hidden="true">&times;</span>
+
         </a>
-        <div class="filter-container collapse <?php if($filters || $is_expanded){ ?>show<?php } ?>" id="toggleFilter">
+
+        <div id="toggleFilter" class="collapse<?php if($filters || $is_expanded){ ?> show<?php } ?> filter-collapsed filter-container">
+
             <div class="card-body">
-            <form action="<?php echo $form_url; ?>" method="get">
-                <?php echo html_input('hidden', 'page', 1); ?>
-                <?php if(!empty($ext_hidden_params)){ ?>
-                    <?php foreach($ext_hidden_params as $fname => $fvalue){ ?>
-                        <?php echo html_input('hidden', $fname, $fvalue); ?>
-                        <?php if($filters){ $filters[$fname] = $fvalue; } ?>
+
+                <form action="<?php echo $form_url; ?>" method="get">
+
+                    <?php echo html_input('hidden', 'page', 1); ?>
+
+                    <?php if(!empty($ext_hidden_params)){ ?>
+
+                        <?php foreach($ext_hidden_params as $fname => $fvalue){ ?>
+
+                            <?php echo html_input('hidden', $fname, $fvalue); ?>
+                            <?php if($filters){ $filters[$fname] = $fvalue; } ?>
+
+                        <?php } ?>
+
                     <?php } ?>
-                <?php } ?>
-                <div class="fields form-row">
-                    <?php $fields_count = 0; ?>
-                    <?php foreach($fields as $name => $field){ ?>
-                        <?php if (!$field['is_in_filter']){ continue; } ?>
-                        <?php if (!empty($field['filter_view']) && !$user->isInGroups($field['filter_view'])) { continue; } ?>
-                        <?php $value = isset($filters[$name]) ? $filters[$name] : null; ?>
-                        <?php $output = $field['handler']->setItem(array('ctype_name' => $css_prefix, 'id' => null))->getFilterInput($value); ?>
-                        <?php if (!$output){ continue; } ?>
-                        <?php $fields_count++; ?>
-                        <div class="col-6 form-group field ft_<?php echo $field['type']; ?> f_<?php echo $field['name']; ?>">
-                            <label class="title"><?php echo $field['title']; ?></label>
-                            <div class="value">
-                                <?php echo $output; ?>
-                            </div>
-                        </div>
-                    <?php } ?>
-                    <?php if (!empty($props_fields)){ ?>
-                        <?php foreach($props as $prop){ ?>
+
+                    <div class="form-row fields">
+
+                        <?php $fields_count = 0; ?>
+
+                        <?php foreach($fields as $name => $field){ ?>
+
                             <?php
-                                if (!$prop['is_in_filter']){ continue; }
+                                if (!$field['is_in_filter']){ continue; }
+                                if (!empty($field['filter_view']) && !$user->isInGroups($field['filter_view'])) { continue; }
+                                $value = isset($filters[$name]) ? $filters[$name] : null;
+                                $output = $field['handler']->setItem(array('ctype_name' => $css_prefix, 'id' => null))->getFilterInput($value);
+                                if (!$output){ continue; }
                                 $fields_count++;
-                                $field = $props_fields[$prop['id']];
-                                $field->setName("p{$prop['id']}");
-                                if ($prop['type'] == 'list' && !empty($prop['options']['is_filter_multi'])){ $field->setOption('filter_multiple', true); }
-                                if ($prop['type'] == 'number' && !empty($prop['options']['is_filter_range'])){ $field->setOption('filter_range', true); }
-                                $value = isset($filters["p{$prop['id']}"]) ? $filters["p{$prop['id']}"] : null;
                             ?>
-                            <div class="col-6 form-group field ft_<?php echo $prop['type']; ?> f_prop_<?php echo $prop['id']; ?>">
-                                <label class="title"><?php echo $prop['title']; ?></label>
-                                <div class="value">
-                                    <?php echo $field->getFilterInput($value); ?>
-                                </div>
+
+                            <div class="form-group col-6 field ft_<?php echo $field['type']; ?> f_<?php echo $field['name']; ?>">
+                                <label for="<?php echo $field['handler']->element_name; ?>" class="title"><?php echo $field['title']; ?></label>
+                                <div class="value"><?php echo $output; ?></div>
                             </div>
+
                         <?php } ?>
-                    <?php } ?>
-                </div>
-                <?php if ($fields_count) { ?>
-                    <div class="form-inline">
-                        <?php echo html_submit(LANG_FILTER_APPLY); ?>
-                        <?php if (sizeof($filters)){ ?>
-                            <a class="btn btn-link" href="<?php echo ((is_array($page_url) && !empty($page_url['cancel'])) ? $page_url['cancel'] : $form_url); ?>"><?php echo LANG_CANCEL; ?></a>
-                            <i class="fa fa-link text-primary pr-1"></i>
-                            <a href="<?php echo $form_url.$form_url_sep.http_build_query($filters); ?>"><?php echo LANG_FILTER_URL; ?></a>
+
+                        <?php if (!empty($props_fields)){ ?>
+
+                            <?php foreach($props as $prop){ ?>
+
+                                <?php
+                                    if (!$prop['is_in_filter']){ continue; }
+                                    $field = $props_fields[$prop['id']];
+                                    $field->setName("p{$prop['id']}");
+                                    if ($prop['type'] == 'list' && !empty($prop['options']['is_filter_multi'])){ $field->setOption('filter_multiple', true); }
+                                    if ($prop['type'] == 'number' && !empty($prop['options']['is_filter_range'])){ $field->setOption('filter_range', true); }
+                                    $value = isset($filters["p{$prop['id']}"]) ? $filters["p{$prop['id']}"] : null;
+                                    $fields_count++;
+                                ?>
+
+                                <div class="form-group col-6 field ft_<?php echo $prop['type']; ?> f_prop_<?php echo $prop['id']; ?>">
+                                    <label class="title"><?php echo $prop['title']; ?></label>
+                                    <div class="value"><?php echo $field->getFilterInput($value); ?></div>
+                                </div>
+
+                            <?php } ?>
+
                         <?php } ?>
+
                     </div>
-                <?php } ?>
-            </form>
+
+                    <?php if ($fields_count) { ?>
+
+                        <div class="form-inline">
+
+                            <?php echo html_submit(LANG_FILTER_APPLY); ?>
+
+                            <?php if (sizeof($filters)){ ?>
+
+                                <a class="btn btn-link" href="<?php echo ((is_array($page_url) && !empty($page_url['cancel'])) ? $page_url['cancel'] : $form_url); ?>"><?php echo LANG_CANCEL; ?></a>
+                                
+                                <a class="btn btn-link" href="<?php echo $form_url.$form_url_sep.http_build_query($filters); ?>"><i class="fa fa-link text-primary pr-1"></i> <?php echo LANG_FILTER_URL; ?></a>
+
+                            <?php } ?>
+
+                        </div>
+
+                    <?php } ?>
+
+                </form>
+
+            </div>
+
         </div>
-        </div>
+
     </div>
+
 </div>
-<?php if (!$fields_count) { ?>
-<script type="text/javascript">
-    $(function (){
-        $('.filter-panel.groups-filter').hide();
-    });
-</script>
-<?php } ?>
